71dd56bff6c993b085050dc13735e92b26e59db3
Marcelo <marcelo@siasp.com.br>
2024-02-09 11:07:10 -0300

    Ranking em tempo real

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastifyCookie from "@fastify/cookie";
+import fastifyWebsocket from "@fastify/websocket";
 import fastify from "fastify";
 import { createPoll } from "./routes/create-polls";
 import { getPoll } from "./routes/get-polls";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
 
@@ -12,9 +14,13 @@ app.register(fastifyCookie, {
   parseOptions: {},
 });
 
+app.register(fastifyWebsocket);
+
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running");
