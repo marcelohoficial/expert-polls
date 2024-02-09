7c235fb33e9063a5b53d5a85fa0f8efb35763ab0
Marcelo <marcelo@siasp.com.br>
2024-02-09 09:39:45 -0300

    Adicionando rotas para votos

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,20 @@
+import fastifyCookie from "@fastify/cookie";
 import fastify from "fastify";
 import { createPoll } from "./routes/create-polls";
 import { getPoll } from "./routes/get-polls";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 
+app.register(fastifyCookie, {
+  secret: "polls",
+  hook: "onRequest",
+  parseOptions: {},
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running");
